8a7a905b9ad5454c9a6aff17bbce958d055ba7b2
atsukanrock <atsukanrock@gmail.com>
2014-07-27 13:00:29 +0900

    Reduce the number of #if-#else-#endif directives in Program.cs for code simplicity.

--- ProducerConsumer.ConsoleApp/Program.cs
+++ ProducerConsumer.ConsoleApp/Program.cs
@@ -3,12 +3,15 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-#if UseBlockingCollection
-using System.Collections.Concurrent;
-#endif
-
 #if !UseBlockingCollection
-using MultithreadDesignPattern.ProducerConsumer;
+using ChannelType = MultithreadDesignPattern.ProducerConsumer.Channel<string>;
+using ProducerType = ProducerConsumer.ConsoleApp.Producer;
+using ConsumerType = ProducerConsumer.ConsoleApp.Consumer;
+
+#else
+using ChannelType = System.Collections.Concurrent.BlockingCollection<string>;
+using ProducerType = ProducerConsumer.ConsoleApp.ProducerBlockingCollection;
+using ConsumerType = ProducerConsumer.ConsoleApp.ConsumerBlockingCollection;
 
 #endif
 
@@ -26,41 +29,23 @@ namespace ProducerConsumer.ConsoleApp
             const int consumerCount = 3;
             const int consumerWorkload = 3000;
 
-#if !UseBlockingCollection
-            var channel = new Channel<string>(channelCapacity);
-            var producers = new List<Producer>();
-            var consumers = new List<Consumer>();
-#else
             // By calling other constructor overloads of BlockingCollection<T>,
             // it becomes possible to switch algorithm of the collection among
             // FIFO (ConcurrentQueue<T>), LIFO (ConcurrentStack<T>) and so on.
-            var channel = new BlockingCollection<string>(channelCapacity);
-            var producers = new List<ProducerBlockingCollection>();
-            var consumers = new List<ConsumerBlockingCollection>();
-#endif
+            var channel = new ChannelType(channelCapacity);
+            var producers = new List<ProducerType>();
+            var consumers = new List<ConsumerType>();
 
             for (int i = 0; i < producerCount; i++)
             {
-                var producer =
-#if !UseBlockingCollection
-                    new Producer(
-#else
-                    new ProducerBlockingCollection(
-#endif
-                        "Producer#" + i, producerWorkload, channel);
+                var producer = new ProducerType("Producer#" + i, producerWorkload, channel);
                 Task.Run(() => producer.Run());
                 producers.Add(producer);
             }
 
             for (int i = 0; i < consumerCount; i++)
             {
-                var consumer =
-#if !UseBlockingCollection
-                    new Consumer(
-#else
-                    new ConsumerBlockingCollection(
-#endif
-                        "Consumer#" + i, consumerWorkload, channel);
+                var consumer = new ConsumerType("Consumer#" + i, consumerWorkload, channel);
                 Task.Run(() => consumer.Run());
                 consumers.Add(consumer);
             }
@@ -70,6 +55,7 @@ namespace ProducerConsumer.ConsoleApp
             consumers.ForEach(c => c.Shutdown());
             Console.ReadLine();
 #if UseBlockingCollection
+            // BlockingCollection<T> implements IDisposable.
             channel.Dispose();
 #endif
         }
