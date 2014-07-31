ebcdbebae4389a3cffc9ca263cf2c8a6aee434ee
atsukanrock <atsukanrock@gmail.com>
2014-07-31 15:11:23 +0900

    Install Microsoft.Owin.Cors to ImageProcessor.Web project and enable CORS for the web site.

--- ImageProcessor.Web/Startup.cs
+++ ImageProcessor.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using ImageProcessor.Web;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 
 [assembly: OwinStartup(typeof(Startup))]
@@ -11,6 +12,7 @@ namespace ImageProcessor.Web
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll);
             app.MapSignalR();
         }
     }
