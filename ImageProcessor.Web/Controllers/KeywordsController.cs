b12c3a54141ba0bbbe39d5ffe26bfe9e1b858f9c
atsukanrock <atsukanrock@gmail.com>
2014-08-02 05:24:18 +0900

    Use Web API instead of SignalR client in web client because it does not need always connected to server.

---
+++ ImageProcessor.Web/Controllers/KeywordsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using ImageProcessor.Web.Hubs;
+using Microsoft.AspNet.SignalR;
+
+namespace ImageProcessor.Web.Controllers
+{
+    public class KeywordsController : ApiController
+    {
+        public void Post([FromBody]string keyword)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<KeywordHub>();
+            context.Clients.All.addPostedKeyword(keyword);
+        }
+    }
+}
